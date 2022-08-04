0b1bda2252e5d39647e7beb23bafbfb2af9f6866
Jake Doan <jakedoan3@gmail.com>
2022-08-03 20:40:35 -0500

    new bio

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -4,13 +4,18 @@ import Contact from "./Contact"
 const AboutMe = () => {
   return (
     <div>
-      <h1>About Me</h1>
-      <p>In January 2017, I moved to Chicago to begin my professional career after completing my Bachelor's Degree at Indiana University a few months prior. In that time, I've deeply connected with the community here and persistently strive to learn from the perspectives and experiences of others. <br/>Throughout the entirety of my professional life, I have balanced working full-time with my expansive skills and interests, especially during the disruption of the COVID-19 pandemic.</p>
-      <p>From its outset, my goal was to keep my body and mind healthy and continuously improving. I was fortunately able to maintain full-time employment, and eventually honed my desire to grow into becoming a fullstack web developer. </p>
-      <p>I first explored my curiosity by completing Google's IT Support Certification, and was exposed to the prospect of coding and navigating terminals. Despite the daunting task of then learning to code with no foundation, I eventually enrolled and successfully completed FullStack Academy's Web Developer Bootcamp.
-       <br/>While certainly fast-paced and daunting to learn, web development has already become one of my most rewarding and exciting pursuits; it almost feels as if I've gained a superpower I'm thrilled to wield. </p>
-      <p>During and since my completion of this course, I have worked on several projects and am thrilled to continue my work. </p>
-      <button onClick={()=>{}}>Contact Me</button>
+      <div id="bio-full">
+        <h2>My name is Jake</h2>
+        <p>In January 2017, I moved from Memphis, Tennessee to Chicago, Illinois, to begin my professional career and have loved every day since. I love creative expression, particularly through music and digital media, and always aspire to make a positive impact in doing so.</p>
+        <p>During the COVID-19 pandemic, I successfully completed FullStack Academy’s Web Development Certification program. Coding feels like a new superpower I’ve obtained, and I love the sense of discovery each project now offers. I’m always fascinated by each app providing a visual reflection for what I’ve learned and where I can continue to grow.</p>
+        <p>Like any responsible superpower wielder, I intend to develop applications with the greater good of others in mind. The long uncertainty of COVID-19 illuminated many opportunities to help those in need, and that there are countless possible avenues for doing so with web development. I look forward to where I arrive through exploring these possibilities.</p>
+        <button onClick={()=>{<Contact/>}}>Contact Me</button>
+        <button onClick={()=>{}}>Download Resume</button>
+      </div>
+      <div id="skills">
+        <h2>Skills</h2>
+      </div>
+      
       
     </div>
   )
--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,8 @@ const Home = () => {
   return (
     <div>
         <h1>James "Jake" Doan III</h1>
-        {/* Google Doc Resume Link */}
+        <h3>I am an aspiring web developer based in Chicago, Illinois. I create websites and web-based applications and carefully blend creative concepts with high-powered performance with each project I build.</h3>
+        <button onClick={()=>{}}>Resume</button>
     </div>
   )
 }
