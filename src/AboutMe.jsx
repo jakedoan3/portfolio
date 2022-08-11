77345950bcadbe9084c8db4d384c5381f79614b4
Jake Doan <jakedoan3@gmail.com>
2022-08-10 19:49:06 -0500

    footer links

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
     <div>
       <div id="bio-full">
         <h2>My name is Jake</h2>
-        <p>In January 2017, I moved from Memphis, Tennessee to Chicago, Illinois, to begin my professional career and have loved every day since. I love creative expression, particularly through music and digital media, and always aspire to make a positive impact in doing so.</p>
+        <p>In January 2017, I moved from Memphis, Tennessee to Chicago, Illinois, to begin my career and have embraced every opportunity to grow personally and professionally. I love creative expression, particularly through music and digital media, and always aspire to make a positive impact in doing so.</p>
         <p>During the COVID-19 pandemic, I successfully completed FullStack Academy’s Web Development Certification program. Coding feels like a new superpower I’ve obtained, and I love the sense of discovery each project now offers. I’m always fascinated by each app providing a visual reflection for what I’ve learned and where I can continue to grow.</p>
         <p>Like any responsible superpower wielder, I intend to develop applications with the greater good of others in mind. The long uncertainty of COVID-19 illuminated many opportunities to help those in need, and that there are countless possible avenues for doing so with web development. I look forward to where I arrive through exploring these possibilities.</p>
         <button onClick={()=>{<Contact/>}}>Contact Me</button>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <p>Linkedin</p>
-      <p>Github</p>
+      <a href="https://www.linkedin.com/in/james-doan-iii/">
+        <img src="/icons8-linkedin-80.png" alt="Linkedin" width="25vw"/>
+        Linkedin
+        </a>
+      <a href="https://github.com/jakedoan3">
+        <img src="/icons8-github-80.png" alt="GitHub" width="25vw"/>
+        Github
+        </a >
     </div>
   )
 }
